0ad171e38d9ab070ea96275f6a91b8b39f3680e4
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-04-04 18:07:34 +0530

    Fix OAuth2 login errors and improve success handler safety

--- src/main/java/security/OAuth2SuccessHandler.java
+++ src/main/java/security/OAuth2SuccessHandler.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.*;
 import model.entity.User;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import repository.UserRepository;
