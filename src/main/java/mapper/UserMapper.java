bcaa13ad749d68b73ae2de446f6c48888e56e336
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-04-04 23:24:45 +0530

    Implement UserService with OAuth user handling, category assignment, and profile retrieval

---
+++ src/main/java/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package mapper;
+
+import model.dto.UserDTO;
+import model.entity.Category;
+import model.entity.User;
+
+import java.util.stream.Collectors;
+
+public final class UserMapper {
+
+
+
+}
--- src/main/java/repository/CategoryRepository.java
+++ src/main/java/repository/CategoryRepository.java
@@ -1,8 +1,14 @@
 package repository;
 
 import model.dto.CategoryDTO;
+import model.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Set;
+
 public interface CategoryRepository extends JpaRepository<CategoryDTO, Long> {
+
+    List<Category> findById(Set<Long> ids);
 
 }
