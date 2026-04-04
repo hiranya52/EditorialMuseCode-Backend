1ac6d4acaf512d083605ae21515f82eb2579388d
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-04-04 17:39:51 +0530

    Create UserEntity with OAuth2 support and category relationship mapping

---
+++ src/main/java/model/entity/User.java
@@ -0,0 +1,38 @@
+package model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fullName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    private String provider; // LOCAL, GOOGLE, GITHUB
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "user_categories",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private Set<CategoryEntity> categories;
+
+}
