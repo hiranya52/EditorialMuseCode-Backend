fea4e8ffb92aa9726ab933a270570359bd6c3fc8
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-04-04 17:44:25 +0530

    Add Category entity with many-to-many relationship to User

---
+++ src/main/java/model/entity/Category.java
@@ -0,0 +1,27 @@
+package model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    // Optional: reverse mapping (User ↔ Category)
+    @ManyToMany(mappedBy = "categories")
+    private Set<User> users;
+
+}
