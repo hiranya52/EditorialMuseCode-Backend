65bcc5fd406102347a3671283237c2411500340b
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-05-06 16:45:07 +0530

    Remove test endpoint from AuthController

--- src/main/java/edu/icet/controller/AuthController.java
+++ src/main/java/edu/icet/controller/AuthController.java
@@ -15,22 +15,6 @@ public class AuthController {
     @Autowired
     private AuthService service;
 
-    @GetMapping
-    private String test(){
-        return "MuseCode Loaded....";
-    }
-
-//    @PostMapping("/register")
-//    public String register(@RequestBody AuthRequest request) {
-//        return service.register(request.getUsername(), request.getPassword());
-//    }
-//
-//    @PostMapping("/login")
-//    public String login(@RequestBody AuthRequest request) {
-//        return service.login(request.getUsername(), request.getPassword());
-//    }
-
-
     @PostMapping("/register")
     public String register(@RequestBody RegisterRequest request) {
         return service.register(
