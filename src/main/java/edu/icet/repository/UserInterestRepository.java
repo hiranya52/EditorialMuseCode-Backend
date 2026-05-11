f5a0a04b94d5225cf30269c77a7057eed0fb405e
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-05-11 16:41:56 +0530

    Add UserInterestRepository for user interest operations

---
+++ src/main/java/edu/icet/repository/UserInterestRepository.java
@@ -0,0 +1,10 @@
+package edu.icet.repository;
+
+import edu.icet.model.entity.UserInterest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserInterestRepository extends JpaRepository<UserInterest, Long> {
+
+
+
+}
