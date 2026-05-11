3fbf73f65e540b985ba5fad01d79bee3b356bb1e
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-05-11 16:41:05 +0530

    Add findByName method to CategoryRepository

--- src/main/java/edu/icet/repository/CategoryRepository.java
+++ src/main/java/edu/icet/repository/CategoryRepository.java
@@ -3,7 +3,10 @@ package edu.icet.repository;
 import edu.icet.model.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
+    Optional<Category> findByName(String name);
 
 }
