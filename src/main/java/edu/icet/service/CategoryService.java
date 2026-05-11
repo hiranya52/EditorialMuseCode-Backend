f59e7fa776d9ae581faae3ab3bf8bc058258cdf8
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-05-11 22:47:13 +0530

    Implement save functionality for categories

--- src/main/java/edu/icet/service/CategoryService.java
+++ src/main/java/edu/icet/service/CategoryService.java
@@ -1,12 +1,26 @@
 package edu.icet.service;
 
+import edu.icet.model.dto.CategoryRequestDTO;
+import edu.icet.model.entity.Category;
+import edu.icet.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class CategoryService {
 
+    private final CategoryRepository categoryRepository;
 
+    public Category createCategory(CategoryRequestDTO dto) {
+
+        Category category = Category.builder()
+                .name(dto.getName())
+                .build();
+
+        return categoryRepository.save(category);
+    }
 
 }
