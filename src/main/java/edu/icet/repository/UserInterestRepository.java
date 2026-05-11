54e84b1242fdecaea7aef5e4a04781ded7264ebf
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-05-11 16:44:18 +0530

    Add findByUserId method to UserInterestRepository

--- src/main/java/edu/icet/repository/UserInterestRepository.java
+++ src/main/java/edu/icet/repository/UserInterestRepository.java
@@ -3,8 +3,10 @@ package edu.icet.repository;
 import edu.icet.model.entity.UserInterest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserInterestRepository extends JpaRepository<UserInterest, Long> {
 
-
+    List<UserInterest> findByUserId(Long userId);
 
 }
