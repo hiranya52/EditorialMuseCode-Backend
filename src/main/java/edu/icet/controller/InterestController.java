cf0b9b6cdf6b9cbabc2f1b09d224fd7ffc549e76
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-05-11 23:07:18 +0530

    Add endpoint to save user interests in InterestController

--- src/main/java/edu/icet/controller/InterestController.java
+++ src/main/java/edu/icet/controller/InterestController.java
@@ -1,14 +1,23 @@
 package edu.icet.controller;
 
+import edu.icet.model.dto.InterestRequestDTO;
+import edu.icet.service.InterestService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/interests")
 @RequiredArgsConstructor
 public class InterestController {
 
+    private final InterestService interestService;
 
+    @PostMapping
+    public String saveInterests(@RequestParam Long userId, @RequestBody InterestRequestDTO dto) {
+
+        interestService.saveUserInterests(userId, dto);
+
+        return "Interests Saved Successfully";
+    }
 
 }
