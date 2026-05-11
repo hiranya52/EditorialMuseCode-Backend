ce27a308a6cb80f6327b32c414c103718e6d886c
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-05-11 23:04:15 +0530

    Add createCategory endpoint to CategoryController

--- src/main/java/edu/icet/controller/CategoryController.java
+++ src/main/java/edu/icet/controller/CategoryController.java
@@ -1,6 +1,11 @@
 package edu.icet.controller;
 
+import edu.icet.model.dto.CategoryRequestDTO;
+import edu.icet.model.entity.Category;
+import edu.icet.service.CategoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class CategoryController {
 
+    private final CategoryService categoryService;
 
+    @PostMapping
+    public Category createCategory(@RequestBody CategoryRequestDTO dto) {
+        return categoryService.createCategory(dto);
+    }
 
 }
