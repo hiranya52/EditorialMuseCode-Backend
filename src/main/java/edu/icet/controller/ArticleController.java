a7de977f6317c9b8bdc805559409d66762044ad3
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-05-16 11:11:41 +0530

    Add createArticle endpoint in ArticleController

--- src/main/java/edu/icet/controller/ArticleController.java
+++ src/main/java/edu/icet/controller/ArticleController.java
@@ -1,6 +1,11 @@
 package edu.icet.controller;
 
+import edu.icet.model.dto.ArticleRequestDTO;
+import edu.icet.model.entity.Article;
+import edu.icet.service.ArticleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ArticleController {
 
+    private final ArticleService articleService;
 
+    @PostMapping
+    public Article createArticle(@RequestBody ArticleRequestDTO dto) {
+        return articleService.createArticle(dto, 1L);
+    }
 
 }
