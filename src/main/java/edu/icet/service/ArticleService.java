ec422c95534775cdd1079c50238b2bb5d3abf6fb
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-05-16 10:59:39 +0530

    Initialize ArticleService with Spring service annotation

---
+++ src/main/java/edu/icet/service/ArticleService.java
@@ -0,0 +1,12 @@
+package edu.icet.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleService {
+
+
+
+}
