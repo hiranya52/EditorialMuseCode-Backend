70b7bb10d1e624233cf269160bfb4022e55da59e
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-05-16 16:28:52 +0530

    Add CrossOrigin configuration to InterestController

--- src/main/java/edu/icet/controller/InterestController.java
+++ src/main/java/edu/icet/controller/InterestController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/interests")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class InterestController {
 
     private final InterestService interestService;
